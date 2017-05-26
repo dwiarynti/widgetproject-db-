b7b70f9dbf3da34b29fd99fcca682fd63b1f9e49
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-26 15:42:00 +0700

    add getbysite in person ctrl

--- routes/person.js
+++ routes/person.js
@@ -3,23 +3,28 @@ var router = express.Router();
 
 var db = require('./connection');
 var persondb = db.sublevel('person');
+var sitedb = db.sublevel('site');
 
 var person = [
     {
         id:1,
         name:"Budi",
+        siteid:"001"
     },
     {
         id:2,
         name:"Anton",
+        siteid:"001"
     },
     {
         id:3,
         name:"Didi",
+         siteid:"002"
     },
      {
         id:4,
         name:"Ani",
+        siteid:"002"
     }
 ]
 
@@ -67,5 +72,66 @@ router.post('/person/create', function (req, res) {
     if (err) res.json(500,err);
     else res.json({success: true});
 });
+});
+
+router.get('/person/getbysite/:_id',function(req,res)
+{
+    var id = req.params._id;
+    var listitem = [];
+    var sitename = {};
+    sitedb.get('site',function(err,sites)
+    {
+        if(err) res.json(500,err);
+        else
+        for(var i = 0 ; i < sites.length; i++)
+        {
+            var element = sites[i];
+            if(element.id == id)
+            sitename = element.sitename;
+        }
+        if(sitename != "")
+        {
+            persondb.get('person',function(err,dataperson)
+            {
+                 if(err) res.json(500,err);
+               
+                 else
+                  var index = 0;
+                  var item = [];
+                
+                 for(var x = 0 ; x < dataperson.length; x++)
+                 {
+                    var element = dataperson[index];
+                    if(element != null)
+                    {
+                    if(element.siteid == id)
+                    {
+                        var obj =  {};
+                        
+                        obj.sitename = sitename;
+                        obj.id =element.id;
+                        obj.siteid = element.siteid;
+                        obj.name = element.name;
+                        
+                        item.push(obj);
+                    }
+                    }
+                     index+=1;
+                 }
+                 if(index == dataperson.length)
+                 {
+                    res.json({"obj": item })
+                 }
+               
+
+            });
+        }
+        else
+        {
+            res.json(500,err);
+        }
+    });
+
+
 });
 module.exports = router;
