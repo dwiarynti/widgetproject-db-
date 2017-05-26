39cc64d9d4eaf3f3f6702fda6bad0cca6324dcd1
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-26 12:08:33 +0700

    add update in appmanagement cntrl

--- routes/appmanagement.js
+++ routes/appmanagement.js
@@ -60,6 +60,40 @@ router.post('/appmanagement/create',function(req,res)
         }
 
     });
+});
+
+router.post('/appmanagement/update',function(req,res)
+{
+     var listobj = [];
+    
+      appmanagementdb.get('appmanagement',function(err,obj)
+      {
+          if(err) res.json(500,err);
+          else
+          for(var i = 0; i < obj.length; i++)
+          {
+           
+            if(obj[i].id == req.body.id)
+            {
+                obj[i].pagename = req.body.pagename;
+                obj[i].pagestatus = req.body.pagestatus;
+                obj[i].widget = req.body.widget;
+            }
+            listobj.push(obj[i]);
+          }
+         
+          if(listobj.length == obj.length)
+          {
+          appmanagementdb.put('appmanagement',listobj,function(err,data)
+          {
+            if(err)
+            res.json(500,err);
+            else
+            res.json({"success":true});
+          });
+          }
+      });
+
 })
 
 router.get('/appmanagement/getall',function(req,res)
