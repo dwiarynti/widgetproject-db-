a9889c063cc441b6a289168c02fdef03952dc539
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-26 16:39:15 +0700

    add getbysite in device ctrl

--- routes/device.js
+++ routes/device.js
@@ -3,30 +3,35 @@ var router = express.Router();
 
 var db = require('./connection');
 var devicedb = db.sublevel('device');
+var sitedb = db.sublevel('site');
 
 var device = [
     {
         id:1,
         mac: 91290391021,
         devicename:"A",
+        siteid:"001"
        
     },
     {
         id:2,
         mac: 91290391022,
         devicename:"B",
+        siteid:"001"
         
     },
     {
         id:3,
         mac: 91290391023,
         devicename:"C",
+        siteid:"002"
       
     },
     {
         id:4,
         mac: 91290391024,
         devicename:"D",
+        siteid:"002"
        
     }
 ]
@@ -74,6 +79,68 @@ router.get('/device/:_id',function(req,res)
         res.json({"obj": item});
         
     });
+});
+
+router.get('/device/getbysite/:_id',function(req,res)
+{
+    var id = req.params._id;
+    var listitem = [];
+    var sitename = {};
+    sitedb.get('site',function(err,sites)
+    {
+        if(err) res.json(500,err);
+        else
+        for(var i = 0 ; i < sites.length; i++)
+        {
+            var element = sites[i];
+            if(element.id == id)
+            sitename = element.sitename;
+        }
+        if(sitename != "")
+        {
+            devicedb.get('device',function(err,datadevice)
+            {
+                 if(err) res.json(500,err);
+               
+                 else
+                  var index = 0;
+                  var item = [];
+                
+                 for(var x = 0 ; x < datadevice.length; x++)
+                 {
+                    var element = datadevice[index];
+                    if(element != null)
+                    {
+                    if(element.siteid == id)
+                    {
+                        var obj =  {};
+                        
+                        obj.sitename = sitename;
+                        obj.id =element.id;
+                        obj.siteid = element.siteid;
+                        obj.mac = element.mac;
+                        obj.devicename = element.devicename;
+                        
+                        item.push(obj);
+                    }
+                    }
+                     index+=1;
+                 }
+                 if(index == datadevice.length)
+                 {
+                    res.json({"obj": item })
+                 }
+               
+
+            });
+        }
+        else
+        {
+            res.json(500,err);
+        }
+    });
+
+
 });
 
 module.exports = router;
