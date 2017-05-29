4f05d697a99c28839953b316b55157eaeb14f307
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-29 11:51:26 +0700

    add filter in persondevice cntrl

--- routes/persondevice.js
+++ routes/persondevice.js
@@ -18,7 +18,7 @@ var persondevice = [
         id : 2,
         siteid:"001",
         personid:2,
-        deviceid:2,
+        deviceid:1,
     },
     {
         id : 3,
@@ -335,9 +335,166 @@ router.get('/persondevice/:_id',function(req,res)
 
 });
 
-router.get('persondevice/filter',function(req,res)
+router.get('/persondevice/filter/:_id',function(req,res)
 {
+    var id = req.params._id;
+    var paramsdevicename = "A";
+    var listitem = [];
+    var sitename = {};
+    sitedb.get('site',function(err,sites)
+    {
+        if(err)
+        if(err.message == "Key not found in database")
+        {
+            res.json({"success": true, "message": "no data" , "obj": []});
+        }
+        else
+        {
+              res.json(500,err);
+        }
+        else
+        for(var i = 0 ; i < sites.length; i++)
+        {
+            var element = sites[i];
+            if(element.id == id)
+            sitename = element.sitename;
+        }
+        if(sitename != "")
+        {
+            persondevicedb.get('persondevice',function(err,datapersondevice)
+            {
+                if(err)
+                if(err.message == "Key not found in database")
+                {
+                    res.json({"success": true, "message": "no data" , "obj": []});
+                }
+                else
+                {
+                    res.json(500,err);
+                }
+               
+                 else
+                 var index = 0;
+                 var countdevice = 0;
+                 var item = [];
+                 var listpersondevice = [];
+                 var listpersondevicename = [];
+                 var result = [];
+                 for(var x = 0 ; x < datapersondevice.length; x++)
+                 {
+                    var element = datapersondevice[x];
+                    if(element != null)
+                    {
+                    if(element.siteid == id)
+                    {
+                        var obj =  {};
+                        obj.id =element.id;
+                        obj.siteid = element.siteid;
+                        obj.sitename = sitename;
+                        obj.personid = element.personid;
+                        obj.deviceid = element.deviceid;
+                        
+                        listpersondevice.push(obj);
+                    }
+                    }
+                     index+=1;
+                 }
+                
+                if(index == datapersondevice.length)
+                {
+                    
+                  devicedb.get('device',function(err,datadevice)
+                  {
+                    if(err)
+                        if(err.message == "Key not found in database")
+                        {
+                            res.json({"success": true, "message": "no data" , "obj": []});
+                        }
+                        else
+                        {
+                            res.json(500,err);
+                        }
+                      else
+                      for(var i = 0 ; i < datadevice.length; i++)
+                      {
+                         
+                          for(var j = 0; j < listpersondevice.length; j++)
+                          {
+                              if(datadevice[i].id == listpersondevice[j].deviceid)
+                              {
+                                    var obj =  {};
+                                    obj.id    =   listpersondevice[j].id;
+                                    obj.siteid = listpersondevice[j].siteid;
+                                    obj.sitename = listpersondevice[j].sitename;
+                                    obj.personid = listpersondevice[j].personid;
+                                    obj.deviceid = listpersondevice[j].deviceid;
+                                    obj.devicename = datadevice[i].devicename;
+                                    listpersondevicename.push(obj);
+                              }
+                          }
+                      }
+                      persondb.get('person',function(err,dataperson)
+                      {
+                          if(err)
+                           if(err.message == "Key not found in database")
+                            {
+                                res.json({"success": true, "message": "no data" , "obj": []});
+                            }
+                            else
+                            {
+                                res.json(500,err);
+                            }
+                          else
+                          for(var a = 0 ; a < dataperson.length ; a++ )
+                          {
+                              for(var b = 0 ; b < listpersondevicename.length; b++)
+                              {
+                                  if(dataperson[a].id == listpersondevicename[b].personid)
+                                  {
+                                    var obj =  {};
+                                    obj.id    =   listpersondevicename[b].id;
+                                    obj.siteid = listpersondevicename[b].siteid;
+                                    obj.sitename = listpersondevicename[b].sitename;
+                                    obj.personid = listpersondevicename[b].personid;
+                                    obj.deviceid = listpersondevicename[b].deviceid;
+                                    obj.devicename = listpersondevicename[b].devicename;
+                                    obj.personname = dataperson[a].name;
 
+                                    result.push(obj);
+                                  }
+                              }
+                          }
+                          var resultpersondevice =[];
+                          if(paramsdevicename != null)
+                          {
+                            for(var j = 0 ; j < result.length ; j++)
+                            {
+                                if(result[j].devicename == paramsdevicename)
+                                {
+                                    resultpersondevice.push(result[j]);
+                                }
+                            }
+                             res.json({"success":true,"obj": resultpersondevice});
+                          }
+                          else
+                          {
+                            res.json({"success":true,"obj": result});
+                          }
+                           
+
+
+                      })
+                     
+                  });
+                    
+                }
+            });
+        }
+        else
+        {
+            res.json(500,err);
+        }
+    });
 })
 
 module.exports = router;
